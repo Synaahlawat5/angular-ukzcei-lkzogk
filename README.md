fc7815839640307a4454f907c15277e91b4b24f5
Synaahlawat5 <synaahlawat5@gmail.com>
2021-07-26 00:11:42 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ukzcei-lkzogk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ukzcei-lkzogk)
---
+++ src/app/hero.ts
@@ -0,0 +1,29 @@
+export interface Hero {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
